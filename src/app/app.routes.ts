b63b426ada12f31510319368f23940d156199786
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2024-07-18 18:06:36 +0530

    Set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ManageEmpComponent } from './pages/manage-emp/manage-emp.component';
+import { ViewAllEmpComponent } from './pages/view-all-emp/view-all-emp.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "add-employee",
+        component: ManageEmpComponent
+    },
+    {
+        path: "view-all-employee",
+        component: ViewAllEmpComponent
+    }
+];
