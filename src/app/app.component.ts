8f3d5196d5465fc052ae066aaf371e10320a6ca6
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2024-07-18 18:12:39 +0530

    Implemented header with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageEmpComponent } from './pages/manage-emp/manage-emp.component';
 import { ViewAllEmpComponent } from './pages/view-all-emp/view-all-emp.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ManageEmpComponent, ViewAllEmpComponent],
+  imports: [RouterOutlet, ManageEmpComponent, ViewAllEmpComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
