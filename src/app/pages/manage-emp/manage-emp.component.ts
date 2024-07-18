c043bfd231e4bd125f8cc57fc14d024e5b9e4e41
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2024-07-18 15:28:57 +0530

    Save employee record to database

--- src/app/pages/manage-emp/manage-emp.component.ts
+++ src/app/pages/manage-emp/manage-emp.component.ts
@@ -1,23 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-emp',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
 export class ManageEmpComponent {
+  constructor(private http:HttpClient){}
+
   public employee = {
-    firstname : undefined,
-    lastname : undefined,
+    firstName : undefined,
+    lastName : undefined,
     email : undefined,
-    depid : undefined,
-    roleid : undefined
+    depId : undefined,
+    roleId : undefined
   }
 
   public saveEmployee(){
-    console.log(this.employee);
+    this.http.post("http://localhost:8080/emp-controller/add-employee", this.employee).subscribe(
+      (data) => {
+        console.log(data);
+      }
+    )
   }
 }
